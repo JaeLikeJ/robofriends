f2b992d114cccbcec7967c948506a32b89357f46
Jae <jae7park@gmail.com>
2019-10-15 18:17:30 -0700

    uploading to master

---
+++ build/precache-manifest.12200c13837545bbd41b8d650e60484c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edda247f5a86d29ae7d3aa47ba0a17b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "007ca526fc52973b063f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d670f3940bd2fa28bb54",
+    "url": "/static/css/main.294a3e82.chunk.css"
+  },
+  {
+    "revision": "007ca526fc52973b063f",
+    "url": "/static/js/2.caf37d44.chunk.js"
+  },
+  {
+    "revision": "d670f3940bd2fa28bb54",
+    "url": "/static/js/main.c5e56026.chunk.js"
+  },
+  {
+    "revision": "720e22d81d6d677b3a62",
+    "url": "/static/js/runtime-main.c26e8b41.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
